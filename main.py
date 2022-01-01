3bb91f8fe2f7acfd79fc9fdd4f9233a1a1ad2bbb
logangator2 <32340228+logangator2@users.noreply.github.com>
2022-01-01 16:05:05 -0700

    Refactoring
    +: Added comments to clarify code and choices in code
    +: Added core while loop for program interaction
    +: Added catches for incorrect inputs to prevent program shutdown and prompt for new input
    ~: Moved variables to location above code blocks for optimization
    ~: Moved squad randomization for logical arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-# weapon, character, locations list DB
+# weapon, character, locations makeshift list DB
 weapons = ["Flatline", "G7", "Hemlok", "R-301", "Havoc", "Prowler",
         "R-99", "Volt", "Devotion", "L-Star", "Longbow",
         "Peacekeeper", "Sentinel", "Charge Rifle", "EVA-8", "Mastiff",
@@ -26,57 +26,78 @@ locations_SP = ["North Pad", "The Wall", "Highpoint", "Lightning Rod", "Checkpoi
         "Cascade Falls", "Command Center", "Thunder Watch", "Storm Center", "The Mill", "Cenote Cave", 
         "Barometer", "Antenna", "Launch Pad", "Ship Fall", "Gale Station", "Fish Farms"]
 
+# main program
 def main():
-    
-    # ask for user input
-    squad = int(input("How many in your squad? "))
-    # FIXME: Need to check for string input, maybe do a while loop or try/catch?
-    
-    # check for incorrect inputs
-    if squad > 3:
-        squad = 3
-    elif squad < 1:
-        squad = 1
-    map = input("KC, WE, O, or SP? ")
 
-    # set variables ahead of time (optimization)
+    # main loop boolean variable
+    looping = True
+
+    # randomizer variables
     weapon1 = None
     weapon2 = None
     character = None
+    map = None
 
+    # iterator for each squad member
     i = 0
 
-    # loop thrice for each squad member
-    for i in range(squad):
-        # determine weapons
-        weapon1 = weapons[random.randint(0, len(weapons) - 1)]
-        weapon2 = weapons[random.randint(0, len(weapons) - 1)]
-        # determine characters
-        character = characters[random.randint(0, len(characters) - 1)]
-        # print formatting
-        print()
-        print(character, " - ", weapon1, ",", weapon2)
-        print()
-        # removes characters already picked
-        characters.remove(character)
+    print("Welcome to the Apex Legends Randomizer!\n")
 
-    # determine landing zone
-    KC_location = locations_KC[random.randint(0, len(locations_KC) - 1)]
-    WE_location = locations_WE[random.randint(0, len(locations_WE) - 1)]
-    O_location = locations_O[random.randint(0, len(locations_O) - 1)]
-    SP_location = locations_SP[random.randint(0, len(locations_SP) - 1)]
+    while(looping):
+        # ask for user input
+        # check if map has already been input, otherwise skip
+        if map == None:
+            # try statement for handling non-number answers
+            try:
+                # accepts floats (larger pool of correct answers), converts to ints for looping later on
+                squad = int(float(input("How many in your squad? ")))
+            except:
+                print("Incorrect input, please try again.")
+                continue
+        
+        # check for incorrect float inputs
+        if squad > 3.0:
+            print("Number over squad limit, assuming 3 players.")
+            squad = 3
+        elif squad < 1.0:
+            print("Number under squad limit, assuming 1 player.")
+            squad = 1
 
-    if map == "KC":
-        print("\nKC: " + KC_location + "\n")
-    elif map == "WE":
-        print("\nWE: " + WE_location + "\n")
-    elif map == "O":
-        print("\nO: " + O_location + "\n")
-    elif map == "SP":
-        print("\nSP: " + SP_location + "\n")
-    else:
-        print("Incorrect input. Please try again.")
-    return
+        # checked differently than the squad number because no translation (str->int) is necessary
+        map = input("Which Map? KC, WE, O, or SP? ")
+
+        # determine landing zone - more calculations, but clearer code
+        KC_location = locations_KC[random.randint(0, len(locations_KC) - 1)]
+        WE_location = locations_WE[random.randint(0, len(locations_WE) - 1)]
+        O_location = locations_O[random.randint(0, len(locations_O) - 1)]
+        SP_location = locations_SP[random.randint(0, len(locations_SP) - 1)]
+
+        if map == "KC":
+            print("\nKC: " + KC_location + "\n")
+        elif map == "WE":
+            print("\nWE: " + WE_location + "\n")
+        elif map == "O":
+            print("\nO: " + O_location + "\n")
+        elif map == "SP":
+            print("\nSP: " + SP_location + "\n")
+        else:
+            print("Incorrect input. Please try again.")
+            continue
+
+        # loop thrice for each squad member
+        for i in range(squad):
+            # determine weapons
+            weapon1 = weapons[random.randint(0, len(weapons) - 1)]
+            weapon2 = weapons[random.randint(0, len(weapons) - 1)]
+            # determine characters
+            character = characters[random.randint(0, len(characters) - 1)]
+            # print formatting
+            print()
+            print(character, " - ", weapon1, ",", weapon2)
+            print()
+            # removes characters already picked
+            characters.remove(character)
+        return
 
 if __name__ == "__main__":
     main()
