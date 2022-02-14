3840ac5da06827466029841789b34f41e386e862
logangator2 <32340228+logangator2@users.noreply.github.com>
2022-02-13 18:12:52 -0600

    tkinter stable build
    ~: refactored some code to allow for global messageboxing
    +: added grid base to restructure UI
    +: added functioning randomizer button
    +: added functioning messagebox randomization reporter

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ Code written by Maxwell Logan
 Referenced code from https://www.pythontutorial.net/tkinter/ tutorials
 """
 
+from email import message
 import random
 import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
 
 # weapon, character, locations makeshift list DB
 weapons = ["Flatline", "G7", "Hemlok", "R-301", "Havoc", "Prowler",
@@ -32,49 +35,51 @@ locations_SP = ["North Pad", "The Wall", "Highpoint", "Lightning Rod", "Checkpoi
         "Cascade Falls", "Command Center", "Thunder Watch", "Storm Center", "The Mill", "Cenote Cave", 
         "Barometer", "Antenna", "Launch Pad", "Ship Fall", "Gale Station", "Fish Farms"]
 
+# root window for tkinter
+root = tk.Tk()
+root.title("Apex Legends Drop Randomizer")
+
 # main program
 def main():
 
-    # root window for tkinter
-    root = tk.Tk()
-    root.title("Apex Legends Drop Randomizer")
-
-    # # grid layout for the input frame
-    # frame = tk.Frame(root)
-    # frame.columnconfigure(0, weight=1)
-    # frame.columnconfigure(0, weight=3)
+    # grid layout for the input frame
+    frame = ttk.Frame(root)
+    frame.columnconfigure(0, weight=1)
+    frame.columnconfigure(0, weight=1)
 
     # welcome message
-    message = tk.Label(root, text="Welcome to the Unofficial Apex Legends Drop Randomizer!")
-    message.pack()
+    message = ttk.Label(root, 
+        text="Welcome to the Unofficial Apex Legends Drop Randomizer!").grid(column=1, row=0)
+
+    # squad selection label
+    squadnum = ttk.Label(root, text="How many people in your squad?").grid(column=0, row=1)
 
     # squad size radio selection widget
     selected = tk.StringVar()
-    r1 = tk.Radiobutton(root, text='1', value='Value 1', variable=selected)
-    r2 = tk.Radiobutton(root, text='2', value='Value 2', variable=selected)
-    r3 = tk.Radiobutton(root, text='3', value='value 3', variable=selected)
-    r1.pack()
-    r2.pack()
-    r3.pack()
+    r1 = ttk.Radiobutton(root, text='1', value=1, variable=selected).grid(column=0, row=2)
+    r2 = ttk.Radiobutton(root, text='2', value=2, variable=selected).grid(column=0, row=3)
+    r3 = ttk.Radiobutton(root, text='3', value=3, variable=selected).grid(column=0, row=4)
+
+    # map selection label
+    maps = ttk.Label(root, text="Which map are you playing on?").grid(column=2, row=1)
 
     # map radio selection widget
     selected2 = tk.StringVar()
-    r4 = tk.Radiobutton(root, text="King's Canyon", value='Value 1', variable=selected2)
-    r5 = tk.Radiobutton(root, text="World's Edge", value='Value 2', variable=selected2)
-    r6 = tk.Radiobutton(root, text='Olympus', value='value 3', variable=selected2)
-    r7 = tk.Radiobutton(root, text='Storm Point', value='value 4', variable=selected2)
-    r4.pack()
-    r5.pack()
-    r6.pack()
-    r7.pack()
+    r4 = ttk.Radiobutton(root, 
+        text="King's Canyon", value="King's Canyon", variable=selected2).grid(column=2, row=2)
+    r5 = ttk.Radiobutton(root, 
+        text="World's Edge", value="World's Edge", variable=selected2).grid(column=2, row=3)
+    r6 = ttk.Radiobutton(root, 
+        text='Olympus', value='Olympus', variable=selected2).grid(column=2, row=4)
+    r7 = ttk.Radiobutton(root, 
+        text='Storm Point', value='Storm Point', variable=selected2).grid(column=2, row=5)
 
     # randomize button
-    randomize = tk.Button(root, text="Randomize!", command=lambda: randomize(selected, selected2))
-    randomize.pack()
+    randomizer = ttk.Button(root, 
+        text="Randomize!", command=lambda: randomize(int(selected.get()), selected2.get())).grid(column=1, row=6)
 
     # exit button
-    exit = tk.Button(text="Quit", command=root.destroy)
-    exit.pack()
+    exit = ttk.Button(text="Quit", command=root.destroy).grid(column=1, row=10)
 
     # final tkinter loop
     root.mainloop()
@@ -84,7 +89,7 @@ def randomize(squad_size, map_name):
     """
     randomize - core function for the drop randomizer
     **args:
-        squad_size - number of teammates on user's squad, including themselves
+        squad_size - str number of teammates on user's squad, including themselves
         map_name - name of the map that the game is being played on
     """
 
@@ -92,20 +97,25 @@ def randomize(squad_size, map_name):
     weapon1 = None
     weapon2 = None
     character = None
-    map = None
+    character_str = None
+    drop_location = None
+    output_str = ""
 
     # iterator for each squad member
     i = 0
 
-    if map_name == "KC":
-        KC_location = locations_KC[random.randint(0, len(locations_KC) - 1)]
-        # FIXME: Need to add the location to the output
-    elif map_name == "WE":
-        WE_location = locations_WE[random.randint(0, len(locations_WE) - 1)]
-    elif map_name == "O":
-        O_location = locations_O[random.randint(0, len(locations_O) - 1)]
-    elif map_name == "SP":
-        SP_location = locations_SP[random.randint(0, len(locations_SP) - 1)]
+    # determine drop location for selected map
+    if map_name == "King's Canyon":
+        drop_location = "King's Canyon Drop Location:\n" + " " + locations_KC[random.randint(0, len(locations_KC) - 1)]
+    elif map_name == "World's Edge":
+        drop_location = "World's Edge Drop Location:\n" + " " + locations_WE[random.randint(0, len(locations_WE) - 1)]
+    elif map_name == "Olympus":
+        drop_location = "Olympus Drop Location:\n" + " " + locations_O[random.randint(0, len(locations_O) - 1)]
+    elif map_name == "Storm Point":
+        drop_location = "Storm Point Drop Location:\n" + " " + locations_SP[random.randint(0, len(locations_SP) - 1)]
+
+    # add drop location to output_str
+    output_str += drop_location + "\n"
 
     # loop thrice for each squad member
     for i in range(squad_size):
@@ -114,14 +124,17 @@ def randomize(squad_size, map_name):
         weapon2 = weapons[random.randint(0, len(weapons) - 1)]
         # determine characters
         character = characters[random.randint(0, len(characters) - 1)]
-        # print formatting
-        print()
-        print(character, " - ", weapon1, ",", weapon2)
-        print()
+        # formatting
+        character_str = "\n" + character + " - " + weapon1 + ", " + weapon2 + "\n"
         # removes characters already picked
         characters.remove(character)
+        # add character_str to output message
+        output_str += character_str
 
-        # FIXME: Need to do some formatting here, add to output
+    # print(output_str)
+
+    # display outbut in tkinter messagebox
+    tk.messagebox.showinfo(title="Randomization", message=output_str)
 
     return
 
